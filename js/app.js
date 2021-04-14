c57675e78dc12ee75e989d5ac302dd44731b4a02
confucianzuoyuan <zuoyuantc@126.com>
2021-04-14 18:40:12 +0800

    update

--- js/app.js
+++ js/app.js
@@ -201,24 +201,38 @@ class App {
 
         this.ng_element = document.getElementById('ng-button');
         this.ng_element.style.display = 'none';
+        this.ok_element = document.getElementById('ok-button');
+        this.ok_element.style.display = 'block';
 
         this.container1_ng_element = document.getElementById('container1-ng-button');
         this.container1_ng_element.style.display = 'none';
+        this.container1_ok_element = document.getElementById('container1-ok-button');
+        this.container1_ok_element.style.display = 'block';
 
         this.container2_ng_element = document.getElementById('container2-ng-button');
         this.container2_ng_element.style.display = 'none';
+        this.container2_ok_element = document.getElementById('container2-ok-button');
+        this.container2_ok_element.style.display = 'block';
 
         this.container3_ng_element = document.getElementById('container3-ng-button');
         this.container3_ng_element.style.display = 'none';
+        this.container3_ok_element = document.getElementById('container3-ok-button');
+        this.container3_ok_element.style.display = 'block';
 
         this.container4_ng_element = document.getElementById('container4-ng-button');
         this.container4_ng_element.style.display = 'none';
+        this.container4_ok_element = document.getElementById('container4-ok-button');
+        this.container4_ok_element.style.display = 'block';
 
         this.container5_ng_element = document.getElementById('container5-ng-button');
         this.container5_ng_element.style.display = 'none';
+        this.container5_ok_element = document.getElementById('container5-ok-button');
+        this.container5_ok_element.style.display = 'block';
 
         this.container6_ng_element = document.getElementById('container6-ng-button');
         this.container6_ng_element.style.display = 'none';
+        this.container6_ok_element = document.getElementById('container6-ok-button');
+        this.container6_ok_element.style.display = 'block';
 
         this.container1_confirm_element = document.getElementById('container1-confirm');
         this.container1_confirm_element.style.display = 'none';
@@ -239,16 +253,89 @@ class App {
         this.container6_confirm_element.style.display = 'none';
 
         this.container1_edit_element = document.getElementById('container1-edit');
-
         this.container2_edit_element = document.getElementById('container2-edit');
-
         this.container3_edit_element = document.getElementById('container3-edit');
-
         this.container4_edit_element = document.getElementById('container4-edit');
-
         this.container5_edit_element = document.getElementById('container5-edit');
-
         this.container6_edit_element = document.getElementById('container6-edit');
+
+        this.container1_edit_element.addEventListener('click', () => {
+            this.container1_confirm_element.style.display = 'block';
+            this.container1_edit_element.style.display = 'none';
+            this.container1_high_element.readOnly = false;
+            this.container1_low_element.readOnly = false;
+        });
+        this.container1_confirm_element.addEventListener('click', () => {
+            this.container1_confirm_element.style.display = 'none';
+            this.container1_edit_element.style.display = 'block';
+            this.container1_high_element.readOnly = true;
+            this.container1_low_element.readOnly = true;
+        });
+
+        this.container2_edit_element.addEventListener('click', () => {
+            this.container2_confirm_element.style.display = 'block';
+            this.container2_edit_element.style.display = 'none';
+            this.container2_high_element.readOnly = false;
+            this.container2_low_element.readOnly = false;
+        });
+        this.container2_confirm_element.addEventListener('click', () => {
+            this.container2_confirm_element.style.display = 'none';
+            this.container2_edit_element.style.display = 'block';
+            this.container2_high_element.readOnly = true;
+            this.container2_low_element.readOnly = true;
+        });
+
+        this.container3_edit_element.addEventListener('click', () => {
+            this.container3_confirm_element.style.display = 'block';
+            this.container3_edit_element.style.display = 'none';
+            this.container3_high_element.readOnly = false;
+            this.container3_low_element.readOnly = false;
+        });
+        this.container3_confirm_element.addEventListener('click', () => {
+            this.container3_confirm_element.style.display = 'none';
+            this.container3_edit_element.style.display = 'block';
+            this.container3_high_element.readOnly = true;
+            this.container3_low_element.readOnly = true;
+        });
+
+        this.container4_edit_element.addEventListener('click', () => {
+            this.container4_confirm_element.style.display = 'block';
+            this.container4_edit_element.style.display = 'none';
+            this.container4_high_element.readOnly = false;
+            this.container4_low_element.readOnly = false;
+        });
+        this.container4_confirm_element.addEventListener('click', () => {
+            this.container4_confirm_element.style.display = 'none';
+            this.container4_edit_element.style.display = 'block';
+            this.container4_high_element.readOnly = true;
+            this.container4_low_element.readOnly = true;
+        });
+
+        this.container5_edit_element.addEventListener('click', () => {
+            this.container5_confirm_element.style.display = 'block';
+            this.container5_edit_element.style.display = 'none';
+            this.container5_high_element.readOnly = false;
+            this.container5_low_element.readOnly = false;
+        });
+        this.container5_confirm_element.addEventListener('click', () => {
+            this.container5_confirm_element.style.display = 'none';
+            this.container5_edit_element.style.display = 'block';
+            this.container5_high_element.readOnly = true;
+            this.container5_low_element.readOnly = true;
+        });
+
+        this.container6_edit_element.addEventListener('click', () => {
+            this.container6_confirm_element.style.display = 'block';
+            this.container6_edit_element.style.display = 'none';
+            this.container6_high_element.readOnly = false;
+            this.container6_low_element.readOnly = false;
+        });
+        this.container6_confirm_element.addEventListener('click', () => {
+            this.container6_confirm_element.style.display = 'none';
+            this.container6_edit_element.style.display = 'block';
+            this.container6_high_element.readOnly = true;
+            this.container6_low_element.readOnly = true;
+        });
 
         this.container1_data_element = document.getElementById('container1-data');
         this.container2_data_element = document.getElementById('container2-data');
@@ -256,6 +343,45 @@ class App {
         this.container4_data_element = document.getElementById('container4-data');
         this.container5_data_element = document.getElementById('container5-data');
         this.container6_data_element = document.getElementById('container6-data');
+
+        this.container1_high_element = document.getElementById('container1-high');
+        this.container1_low_element = document.getElementById('container1-low');
+        this.container2_high_element = document.getElementById('container2-high');
+        this.container2_low_element = document.getElementById('container2-low');
+        this.container3_high_element = document.getElementById('container3-high');
+        this.container3_low_element = document.getElementById('container3-low');
+        this.container4_high_element = document.getElementById('container4-high');
+        this.container4_low_element = document.getElementById('container4-low');
+        this.container5_high_element = document.getElementById('container5-high');
+        this.container5_low_element = document.getElementById('container5-low');
+        this.container6_high_element = document.getElementById('container6-high');
+        this.container6_low_element = document.getElementById('container6-low');
+
+        this.container1_high_element.value = 0;
+        this.container1_low_element.value = 0;
+        this.container2_high_element.value = 0;
+        this.container2_low_element.value = 0;
+        this.container3_high_element.value = 0;
+        this.container3_low_element.value = 0;
+        this.container4_high_element.value = 0;
+        this.container4_low_element.value = 0;
+        this.container5_high_element.value = 0;
+        this.container5_low_element.value = 0;
+        this.container6_high_element.value = 0;
+        this.container6_low_element.value = 0;
+
+        this.container1_high_element.readOnly = true;
+        this.container1_low_element.readOnly  = true;
+        this.container2_high_element.readOnly = true;
+        this.container2_low_element.readOnly  = true;
+        this.container3_high_element.readOnly = true;
+        this.container3_low_element.readOnly  = true;
+        this.container4_high_element.readOnly = true;
+        this.container4_low_element.readOnly  = true;
+        this.container5_high_element.readOnly = true;
+        this.container5_low_element.readOnly  = true;
+        this.container6_high_element.readOnly = true;
+        this.container6_low_element.readOnly  = true;
 
         this.start_element.addEventListener('click', (event) => {
             event.preventDefault();
@@ -277,30 +403,84 @@ class App {
                     container1_data = dataView.getFloat32();
                     this.container1_data_element.value = container1_data;
 
+                    const high1 = this.container1_high_element.value;
+                    const low1 = this.container1_low_element.value;
+                    if (container1_data > high1 || container1_data < low1) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container1_ok_element.style.display = "none";
+                        this.container1_ng_element.style.display = "block";
+                    }
+
                     let container2_data = arr.slice(12, 16).reverse().buffer;
                     dataView = new DataView(container2_data);
                     container2_data = dataView.getFloat32();
                     this.container2_data_element.value = container2_data;
+
+                    const high2 = this.container2_high_element.value;
+                    const low2 = this.container2_low_element.value;
+                    if (container2_data > high2 || container2_data < low2) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container2_ok_element.style.display = "none";
+                        this.container2_ng_element.style.display = "block";
+                    }
 
                     let container3_data = arr.slice(16, 20).reverse().buffer;
                     dataView = new DataView(container3_data);
                     container3_data = dataView.getFloat32();
                     this.container3_data_element.value = container3_data;
 
+                    const high3 = this.container3_high_element.value;
+                    const low3 = this.container3_low_element.value;
+                    if (container3_data > high3 || container3_data < low3) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container3_ok_element.style.display = "none";
+                        this.container3_ng_element.style.display = "block";
+                    }
+
                     let container4_data = arr.slice(20, 24).reverse().buffer;
                     dataView = new DataView(container4_data);
                     container4_data = dataView.getFloat32();
                     this.container4_data_element.value = container4_data;
+
+                    const high4 = this.container4_high_element.value;
+                    const low4 = this.container4_low_element.value;
+                    if (container4_data > high4 || container4_data < low4) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container4_ok_element.style.display = "none";
+                        this.container4_ng_element.style.display = "block";
+                    }
 
                     let container5_data = arr.slice(24, 28).reverse().buffer;
                     dataView = new DataView(container5_data);
                     container5_data = dataView.getFloat32();
                     this.container5_data_element.value = container5_data;
 
+                    const high5 = this.container5_high_element.value;
+                    const low5 = this.container5_low_element.value;
+                    if (container5_data > high5 || container5_data < low5) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container5_ok_element.style.display = "none";
+                        this.container5_ng_element.style.display = "block";
+                    }
+
                     let container6_data = arr.slice(28, 32).reverse().buffer;
                     dataView = new DataView(container6_data);
                     container6_data = dataView.getFloat32();
                     this.container6_data_element.value = container6_data;
+
+                    const high6 = this.container6_high_element.value;
+                    const low6 = this.container6_low_element.value;
+                    if (container6_data > high6 || container6_data < low6) {
+                        this.ok_element.style.display = "none";
+                        this.ng_element.style.display = "display";
+                        this.container6_ok_element.style.display = "none";
+                        this.container6_ng_element.style.display = "block";
+                    }
 
                     chart1.series[0].addPoint(container1_data, true, true);
                     chart1.redraw();
